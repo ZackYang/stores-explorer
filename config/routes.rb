d946e44c6741b4ab7099b74557748c635bc8a336
ZackYang <zackyang214@gmail.com>
2014-03-13 23:32:56 +0800

    adjust the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ StoresExplorer::Application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :searchers
-      resources :stores
+      resources :searchers, only: [:create]
+      resources :stores, only: [:show, :create]
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'stores#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
